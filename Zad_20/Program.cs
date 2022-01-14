582573d868f0a509b5f48f941937008aba8fca71
kowalkowska-just <kowalkowska.just@gmail.com>
2022-01-14 22:15:29 +0100

    Zad_20

---
+++ Zad_20/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = -10;
+            int j = 2;
+            int s = 0;
+
+            while (i <= 10)
+            {
+                s = s + j;
+                j = j + 2;
+                Console.WriteLine("S = {0}, j = {1}, i = {2}", s, j, i);
+                i++;
+            }
+
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
