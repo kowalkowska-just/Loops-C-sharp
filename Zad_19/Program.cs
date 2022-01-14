b6d8411a335263dcc9919e8059321ef0980196d6
kowalkowska-just <kowalkowska.just@gmail.com>
2022-01-14 22:07:10 +0100

    Zad_19

---
+++ Zad_19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 25; i++)
+            {
+                Console.WriteLine("Język C# jest łatwy do opanowania.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
