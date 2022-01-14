9ebde9b4bafb98e63f540831ced304a6d42ac6aa
kowalkowska-just <kowalkowska.just@gmail.com>
2022-01-14 21:44:50 +0100

    Zad_17

---
+++ Zad_17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wartość liczby n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int j = 2;
+            int s = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                s += j;
+                j += 2;
+                Console.WriteLine("S = {0,3}, j = {1,3}, i = {2,3}", s, j, i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
