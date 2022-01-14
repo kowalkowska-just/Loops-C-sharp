224f9dc9b236d5c5674ff20b4fc2a4a520e689c1
kowalkowska-just <kowalkowska.just@gmail.com>
2022-01-14 16:03:00 +0100

    Zad_10

---
+++ Zad_10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wartość liczby h: ");
+            int h = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= h; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0} ", i*j);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
