b89d4a84ed9820a23552bb68505406bb035581ff
kowalkowska-just <kowalkowska.just@gmail.com>
2022-01-14 16:00:00 +0100

    Zad_4

---
+++ Zad_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program pobiera od użytkownika dwie liczby: a i b
+            // Oraz wypisuje wartości parzyste liczb znajdujących się pomiędzy przedziałem <a;b>
+
+            Console.WriteLine("Podaj wartość a: ");
+            int a = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Podaj wartość b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+
+            // Jeżeli a > b to zamien wartości
+
+            if (a > b)
+            {
+                (a, b) = (b, a);
+            }
+
+            for (int i = a; i <= b; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
