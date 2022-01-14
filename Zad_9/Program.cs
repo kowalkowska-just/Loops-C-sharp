0cf047fcb65c9dd3cdbf8c5462ee878e77510f69
kowalkowska-just <kowalkowska.just@gmail.com>
2022-01-14 16:02:16 +0100

    Zad_9

---
+++ Zad_9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wartość liczby n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int suma = 0;
+            int liczaNieparzysta;
+
+            for (int i = 1; i <= n; i++)
+            {
+                liczaNieparzysta = 2 * i - 1;
+                suma += liczaNieparzysta;
+
+                if (i == n)
+                {
+                    Console.Write("{0} ", liczaNieparzysta);
+                } else
+                {
+                    Console.Write("{0} + ", liczaNieparzysta);
+                }
+            }
+
+            Console.Write("= {0}", suma);
+            Console.ReadLine();
+        }
+    }
+}
