fe63fe24843306f96fabdb692f79ac2e71f8f32d
kowalkowska-just <kowalkowska.just@gmail.com>
2022-01-14 16:00:28 +0100

    Zad_5

---
+++ Zad_5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dlug = 80;
+            int dziennyZarobekJanka = 50;
+            int dziennyZarobekKarola = 40;
+            int dzien = 0;
+            int aktualnaSplata = 0;
+
+            do
+            {
+                dzien++;
+                aktualnaSplata += (int)(0.2 * dziennyZarobekJanka + 0.2 * dziennyZarobekKarola);
+                Console.WriteLine("dzień nr: {0}, spłata łączna do tego dnia: {1}", dzien, aktualnaSplata);
+            } while (aktualnaSplata < dlug);
+
+            Console.ReadKey();
+        }
+    }
+}
