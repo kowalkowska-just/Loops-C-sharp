57102d253b166fcbd90b3a0f5084b4d6ef410247
kowalkowska-just <kowalkowska.just@gmail.com>
2022-01-14 16:51:08 +0100

    Zad_13_c

--- Zad_13_c/Program.cs
+++ Zad_13_c/Program.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Zad_13_b
+namespace Zad_13_c
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // While
+            // Do while
             // Program oblicza wartość y = ax + b 
             // Dla x = < -10 ; 10 >
             // Wartość a i b jest wprowadzana przez użytkownika
@@ -24,12 +24,12 @@ namespace Zad_13_b
             int y;
             int x = -10;
 
-            while (x <= 10)
+            do 
             {
                 y = a * x + b;
                 Console.WriteLine("x = {0,8}, y = {1,8}", x, y);
                 x++;
-            }
+            } while (x<=10);
 
             Console.ReadKey();
         }
