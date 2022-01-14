90b2c6024fb72ccbad3bcc610ada45d207ddb3b5
kowalkowska-just <kowalkowska.just@gmail.com>
2022-01-14 16:04:56 +0100

    Zad_13_a

---
+++ Zad_13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pętla for 
+            // Program oblicza wartość y = ax + b 
+            // Dla x = < -10 ; 10 >
+            // Wartość a i b jest wprowadzana przez użytkownika
+
+            Console.WriteLine("Podaj wartość liczby a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Podaj wartość liczby b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int y;
+
+            for (int x = -10 ; x <= 10 ; x++)
+            {
+                y = a * x + b;
+                Console.WriteLine("x = {0,8}, y = {1,8}", x, y);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
