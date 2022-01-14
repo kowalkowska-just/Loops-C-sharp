d1c7f2fbab072cb69664d7dabffff60ef3cb85c2
kowalkowska-just <kowalkowska.just@gmail.com>
2022-01-14 15:59:13 +0100

    Zad_3

---
+++ Zad_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // PĘTLA FOR z wartością n wczytywaną z klawiatury
+            // Program oblicza sumę 1+2+3+...+n
+
+            Console.WriteLine("Podaj wartość n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int s = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Przebieg pętli nr: {0}", i);
+                s += i;
+            }
+
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
