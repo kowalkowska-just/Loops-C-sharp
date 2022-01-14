29fc28ee47f822e522e45d51d458b6ae494ee323
kowalkowska-just <kowalkowska.just@gmail.com>
2022-01-14 23:00:03 +0100

    Zad_22

---
+++ Zad_22/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wartość liczby a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Podaj wartość liczby b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            loop:
+
+            int R = a % b;
+
+            if (a % b == 0)
+            {
+                Console.WriteLine(b);
+            } else
+            {
+                a = b;
+                b = R;
+                Console.WriteLine("a= {0}, b= {1}, r= {2}", a, b, R);
+                goto loop;
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
