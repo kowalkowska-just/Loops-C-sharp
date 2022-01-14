60db07b18beb7bf58b26c720a9d5fee4bc9b76bc
kowalkowska-just <kowalkowska.just@gmail.com>
2022-01-14 15:58:35 +0100

    Zad_2

---
+++ Zad_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // PĘTLA FOR
+            // Program oblicza sumę 1+2+3+...+10
+
+            int s = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Przebieg pętli nr: {0}", i);
+                s += i;
+            }
+
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
