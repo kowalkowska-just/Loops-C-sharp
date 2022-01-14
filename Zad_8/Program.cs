733cf88125ab00da4b0a16beb419a8a8ae81c127
kowalkowska-just <kowalkowska.just@gmail.com>
2022-01-14 16:01:51 +0100

    Zad_8

---
+++ Zad_8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj ile liczb podzielnych przez 7 chcesz wypisać:");
+            int ile = Convert.ToInt32(Console.ReadLine());
+            int liczbyPodzielne;
+
+            for (int i = 0; i < ile; i++)
+            {
+                liczbyPodzielne = i * 7;
+                if (i == ile - 1)
+                {
+                    Console.Write("{0}", liczbyPodzielne);
+                } else
+                {
+                    Console.Write("{0},", liczbyPodzielne);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
