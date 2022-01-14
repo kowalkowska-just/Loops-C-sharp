3dae33c9c6d87b539c5a2f429ab45bf6cdad2b57
kowalkowska-just <kowalkowska.just@gmail.com>
2022-01-14 19:04:59 +0100

    Zad_14

---
+++ Zad_14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jeśli wciszniesz przycisk 'n' zakończysz działanie programu. ");
+            Console.WriteLine("Natomiast jeśli wciśniesz enter, program pokaże Ci godzinę poraz kolejny. \n");
+            DateTime data = DateTime.Now;
+            string n;
+            do
+            {
+                Console.Write(data.ToString("hh:mm:ss"));
+                n = Console.ReadLine();
+            } while ( n != "n");
+        }
+    }
+}
